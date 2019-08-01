4fa76721bf8ccea6411ae6a5edcff91b19696471
pilgrim <35772798+EnoughSensei@users.noreply.github.com>
2019-08-01 09:37:58 +0300

    And time moves again!

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -1,22 +1,6 @@
 extends Node2D
 
-enum TileID {
-	DIRT,
-	WATERED_DIRT,
-	PLOVED_DIRT,
-	WATERED_PLOVED_DIRT,
-	SOWED_DIRT,
-	WATERED_SOWED_DIRT
-}
-
-onready var tilemap = $CanvasLayer/TileMap
-
-func change_cell(cell, id):
-	if cell == 0:
-		
-
 func _process(delta):
-	
 	pass
 
 func _ready():
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,23 +1,8 @@
 extends KinematicBody2D
 
-enum TileID {
-	DIRT,
-	WATERED_DIRT,
-	PLOVED_DIRT,
-	WATERED_PLOVED_DIRT,
-	SOWED_DIRT,
-	WATERED_SOWED_DIRT
-}
-
 onready var tilemap = $Tilemap
 
 const SPEED = 20
-
-var toolbar = [0, 1, 2, 3]
-var toolbar_sprites = []
-
-func _ready():
-	pass
 
 func move():
 	var move_vec = Vector2(0, 0)
@@ -30,8 +15,6 @@ func move():
 
 func _process(delta):
 	move()
-	for i in toolbar:
-		toolbar_sprites.push_back(Sprite.new())
-		# toolbar_sprites[i].set_texture(tilemap.tile_set.tile_get_texture(toolbar[i]))
-		toolbar_sprites[i].set_texture($PlayerSprite.get_texture())
+
+func _ready():
 	pass
