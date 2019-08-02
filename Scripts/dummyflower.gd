b0c35b3aa19592a6a2ed7c758769544f640a1fc7
pilgrim <35772798+EnoughSensei@users.noreply.github.com>
2019-08-02 15:44:19 +0300

    [WORKING] IT JUST WORKS

--- Scripts/dummyflower.gd
+++ Scripts/dummyflower.gd
@@ -1,21 +1,12 @@
-extends dummySeed
-
-class_name geneticStructure
+extends Node
 
 # Declare member variables here. Examples:
-
-#Genes Begin
-var matGenes = initGenes({})
-var patGenes = initGenes({})
-var phenoGenes = initGenes({})
-#Genes End
-
-#Corresponding Plant
-
+# var a = 2
+# var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
