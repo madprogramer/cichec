835b5a08ebe55f2f4cb69694fe38e6f222dadea5
themadprogramer <themadprogramer@gmail.com>
2019-08-15 22:00:19 +0300

    Continued working on economy system, pushing to receive files from other side

---
+++ Scripts/Entities/Shipment.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+const ItemSlotClass = preload("res://Scripts/ItemSlot.gd");
+
+var slotList = Array();
+
+var holdingItem = null
+
+func _ready():
+	for i in range(16):
+		var slot = ItemSlotClass.new(i);
+		slotList.append(slot);
+		add_child(slot);
+	pass # Replace with function body.
+	
+func _input(event):
+	if event is InputEventMouseButton or event is InputEventMouseMotion:
+		if holdingItem != null && holdingItem.picked:
+			holdingItem.rect_global_position = Vector2(event.position.x, event.position.y);
+
+func _gui_input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+		var clickedSlot;
+		for slot in slotList:
+			var slotMousePos = slot.get_local_mouse_position();
+			var slotTexture = slot.texture;
+			var isClicked = slotMousePos.x >= 0 && slotMousePos.x <= slotTexture.get_width() && slotMousePos.y >= 0 && slotMousePos.y <= slotTexture.get_height();
+			if isClicked:
+				clickedSlot = slot;
+		
+		if holdingItem != null:
+			if clickedSlot.item != null:
+				var tempItem = clickedSlot.item;
+				var oldSlot = slotList[slotList.find(holdingItem.itemSlot)];
+				clickedSlot.pickItem();
+				clickedSlot.putItem(holdingItem);
+				holdingItem = null;
+				oldSlot.putItem(tempItem);
+			else:
+				clickedSlot.putItem(holdingItem);
+				holdingItem = null;
+		elif clickedSlot.item != null:
+			holdingItem = clickedSlot.item;
+			clickedSlot.pickItem();
+			holdingItem.rect_global_position = Vector2(event.position.x, event.position.y);
+	pass
