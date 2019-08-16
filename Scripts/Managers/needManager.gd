f81e5c9cdc86ce5f93247e4d46ad73f338f11b62
pilgrim <35772798+EnoughSensei@users.noreply.github.com>
2019-08-17 02:40:53 +0300

    Added needManager and salesManager

---
+++ Scripts/Managers/needManager.gd
@@ -0,0 +1,37 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var needDictionary = {
+	"WaterseekerFlower" : 1
+}
+
+func set_needDictionary(needDictionary):
+	self.needDictionary = needDictionary
+
+func calcFromArray(array):
+	var countDictionary = {}
+	for i in range(array.size()):
+		if countDictionary[array[i].itemName] == null:
+			countDictionary[array[i].itemName] = array[i].count
+		else:
+			countDictionary[array[i].itemName] += array[i].count
+	
+	for key in needDictionary:
+		if countDictionary[key] == null:
+			return -1
+		if countDictionary[key] < needDictionary[key]:
+			return -1
+		countDictionary[key] -= needDictionary[key]
+	
+	return countDictionary
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
